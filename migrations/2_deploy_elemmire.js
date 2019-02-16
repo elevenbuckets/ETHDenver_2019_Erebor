34ded13bac94c195a1ff49e87813bfa6d858e20c
KaiLung Sun <kaisun@elevenbuckets.com>
2019-02-15 22:31:48 -0700

    tidy up truffle migration files

--- migrations/2_deploy_elemmire.js
+++ migrations/2_deploy_elemmire.js
@@ -6,7 +6,13 @@ var nfToken = artifacts.require("NFToken");
 var nfTokenEnumerable = artifacts.require("NFTokenEnumerable");
 var nfTokenMetadata = artifacts.require("NFTokenMetadata");
 var ELEM = artifacts.require("Elemmire");
-// var EreborTrade = artifacts.require("EreborTrade");
+
+var Erebor = artifacts.require("Erebor");
+var ERC20 = artifacts.require("ERC20");
+var StandardToken = artifacts.require("StandardToken");
+var RNT = artifacts.require("RNT");
+var SafeMath = artifacts.require("SafeMath");
+var MemberShip = artifacts.require("MemberShip");
 
 module.exports = function(deployer) {
     deployer.deploy(addressUtils);
@@ -25,7 +31,40 @@ module.exports = function(deployer) {
     deployer.link(addressUtils, ELEM);
     deployer.link(nfTokenEnumerable, ELEM);
     deployer.link(nfTokenMetadata, ELEM);
-    deployer.deploy(ELEM);
+
+    deployer.deploy(SafeMath);
+    deployer.link(SafeMath, [StandardToken, RNT, Erebor]);
+    deployer.deploy(StandardToken);
+    deployer.link(StandardToken, RNT);
+
+    deployer.deploy(ELEM).then( (dELEM) => {
+        let ELEMAddr = ELEM.address;
+        deployer.deploy(MemberShip, ELEM.address).then( () => {
+            return dELEM.setMemberCtrAddr(MemberShip.address).then( () => {
+                return deployer.deploy(RNT).then((iRNT) => {
+                    let RNTAddr = RNT.address;
+                    return deployer.deploy(
+                        Erebor,
+                        '0xa82e7cfb30f103af78a1ad41f28bdb986073ff45b80db71f6f632271add7a32e',
+                        RNTAddr,
+                        ELEMAddr,
+                        MemberShip.address,
+                        {value: '10000000000000000'}).then(() => {
+                            let EreborAddr = Erebor.address;
+                            dELEM.setMining(EreborAddr, 0, (err,r) => {
+                                if (err) { console.trace(err); throw "bad2" };
+                                console.log(`setMining for EreborAddr`)
+                            })
+                            return iRNT.setMining(EreborAddr).then(() => {
+                                return {RNTAddr, EreborAddr}
+                            })
+                        })
+                }).then((result) => {
+                    console.dir(result);
+                }).catch((err) => { console.trace(err); });
+            })
+        })
+    });
 
     // Todo: the EreborTrade
     // deployer.link(ELEM, EreborTrade);
