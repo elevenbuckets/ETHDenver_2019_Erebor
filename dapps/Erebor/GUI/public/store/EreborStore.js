41aa81ef0fd5f782462d091e54bfc462302377c5
liang <zh_l2006@sina.com>
2019-02-16 23:28:43 -0700

    update link address

--- dapps/Erebor/GUI/public/store/EreborStore.js
+++ dapps/Erebor/GUI/public/store/EreborStore.js
@@ -34,11 +34,12 @@ class EreborStore extends _reflux2.default.Store {
 
 		this.linkAddress = address => {
 			this.erebor.address = address;
-			this.erebor.linkAccount(this.erebor.address);
-			this.setState({ address: this.erebor.address });
-			this.erebor.myMemberStatus().then(data => {
-				let memberShipStatus = data[0];
-				this.setState({ memberShipStatus });
+			this.erebor.linkAccount(this.erebor.address).then(() => {
+				this.setState({ address: this.erebor.userWallet });
+				this.erebor.myMemberStatus().then(data => {
+					let memberShipStatus = data[0];
+					this.setState({ memberShipStatus });
+				});
 			});
 		};
 
