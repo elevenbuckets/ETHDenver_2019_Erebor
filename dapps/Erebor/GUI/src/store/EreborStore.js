c3a4c81889e71a2a08f1a70f16776385c6ec6588
jasonlin <jasonlin@elevenbuckets.com>
2019-02-17 03:24:51 -0700

    fix wording

--- dapps/Erebor/GUI/src/store/EreborStore.js
+++ dapps/Erebor/GUI/src/store/EreborStore.js
@@ -25,7 +25,7 @@ class EreborStore extends Reflux.Store {
 				canQuit: true,
 				stateMsg: null,
 				result: null,
-				currentMiningMessages: ["Currently mining, the expected mined time is 10 min,", "Keep going"],
+				currentMiningMessages: ["Welcome To Erebor!"],
 				mining: false,
 				memberShipStatus: "not member",
 				stoneCount: 0
